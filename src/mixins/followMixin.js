5f25105950fc37c26f9fe9cf824b1ec6b23ad5b0
summogiu <ayadrol0117@gmail.com>
2024-08-08 14:39:35 +0800

    upload folder

--- src/mixins/followMixin.js
+++ src/mixins/followMixin.js
@@ -6,15 +6,28 @@ export default {
   },
   watch: {
     '$route.params.category' () {
-      this.followProducts = JSON.parse(localStorage.getItem('followArray'))
+      this.loadFollowProducts()
     },
     '$route.params.productid' () {
-      this.followProducts = JSON.parse(localStorage.getItem('followArray'))
+      this.loadFollowProducts()
     }
   },
   methods: {
+    loadFollowProducts () { // 初始化、載入localStorage
+      try {
+        const storedData = localStorage.getItem('followArray')
+        this.followProducts = storedData ? JSON.parse(storedData) : []
+      } catch (error) {
+        console.error('Error loading from localStorage', error)
+        this.followProducts = []
+      }
+    },
     addFollow (item) { // 增加、移除關注
-      const index = this.followProducts ? this.followProducts.findIndex(followedItem => followedItem.id === item.id) : -1
+      if (!Array.isArray(this.followProducts)) { // 確保可以讀取this.followProducts
+        this.followProducts = []
+      }
+
+      const index = this.followProducts.findIndex(followedItem => followedItem.id === item.id)
 
       if (index !== -1) {
         this.followProducts.splice(index, 1)
@@ -22,7 +35,7 @@ export default {
         this.followProducts.unshift(item)
       }
 
-      try {
+      try { // 更新localStorage
         localStorage.setItem('followArray', JSON.stringify(this.followProducts))
       } catch (error) {
         console.error('Error saving to localStorage', error)
@@ -33,10 +46,6 @@ export default {
     }
   },
   created () {
-    try {
-      this.followProducts = JSON.parse(localStorage.getItem('followArray')) || []
-    } catch (error) {
-      console.error('Error loading from localStorage', error)
-    }
+    this.loadFollowProducts()
   }
 }

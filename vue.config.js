c0764438ee285878614aaebfb5d224908516874a
summogiu <ayadrol0117@gmail.com>
2024-08-08 11:44:04 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+
   css: {
     loaderOptions: {
       sass: {
-        additionalData: `@import "@/assets/helpers/var.scss";` // 將此支scss的變數套用到全域
+        additionalData: '@import "@/assets/helpers/var.scss";'
       }
     }
-  }
+  },
+
+  publicPath: '/vue-cli-api/dist/'
 })
